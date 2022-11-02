74ba443adc212c1f69f87270c8a35061c72c324b
Eduardo Bonelli jr <eduardo.abjr@gmail.com>
2022-11-02 13:27:05 -0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set - env
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commint -m 'New Deployment'
+git push -f https://github.com/EduardoABJR/pokemon-app.git
+
+cd -
